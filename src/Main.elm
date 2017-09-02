b0d4505c338b3eeaa2c0637562895fa7f5f7bbe9
Federico Ragona <federico.ragona@gmail.com>
2017-09-02 15:48:13 +0200

    Build with Bootstrap

--- src/Main.elm
+++ src/Main.elm
@@ -1,106 +1,141 @@
+module Main exposing (..)
+
 import Array exposing (Array, fromList, map)
 import Css
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, span, text)
 import Html.Attributes exposing (class, disabled, style)
 import Html.Events exposing (onClick)
 import Http exposing (getString)
 import Markdown
 import Maybe exposing (Maybe, withDefault)
+import Slides
 import Task
 import Window
 
-import Slides
 
 main : Program Never Model Msg
 main =
-  Html.program {
-    init = init,
-    update = update,
-    subscriptions = subscriptions,
-    view = view
-  }
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
 
-type alias Model = {
-  windowSize : Window.Size,
-  slides : Array(Html Msg),
-  index : Int
-}
 
-type Msg =
-  SlidesNotFound
-  | GotSlides (Array(Html Msg))
-  | PreviousSlide
-  | NextSlide
-  | Resize Window.Size
+type alias Model =
+    { slides : Array (Html Msg)
+    , index : Int
+    }
 
-init : (Model, Cmd Msg)
+
+type Msg
+    = SlidesNotFound
+    | GotSlides (Array (Html Msg))
+    | PreviousSlide
+    | NextSlide
+
+
+init : ( Model, Cmd Msg )
 init =
-  (Model (Window.Size 0 0) Array.empty 0,
-    Task.perform (\x -> Resize x) Window.size)
+    ( Model Array.empty 0, getSlides )
+
 
 getSlides : Cmd Msg
 getSlides =
-  (Http.send
-    (\r ->
-      case r of
-        Result.Err _ ->
-          SlidesNotFound
-        Result.Ok s ->
-          GotSlides <| Slides.parse s)
-    (Http.getString "/slides.md"))
+    Http.send
+        (\r ->
+            case r of
+                Result.Err _ ->
+                    SlidesNotFound
 
-update : Msg -> Model -> (Model, Cmd Msg)
+                Result.Ok s ->
+                    GotSlides <| Slides.parse s
+        )
+        (Http.getString "/slides.md")
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    SlidesNotFound ->
-      (model, Cmd.none)
-    GotSlides slides ->
-      ({ model | slides = slides}, Cmd.none)
-    PreviousSlide ->
-      ({ model | index = model.index - 1 }, Cmd.none)
-    NextSlide ->
-      ({ model | index = model.index + 1 }, Cmd.none)
-    Resize newSize ->
-      ({ model | windowSize = newSize }, getSlides)
+    case msg of
+        SlidesNotFound ->
+            ( model, Cmd.none )
+
+        GotSlides slides ->
+            ( { model | slides = slides }, Cmd.none )
+
+        PreviousSlide ->
+            ( { model | index = model.index - 1 }, Cmd.none )
+
+        NextSlide ->
+            ( { model | index = model.index + 1 }, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Window.resizes Resize
+    Sub.none
 
-renderButtons : Model -> Html Msg
-renderButtons model =
-  div
-    [ class "buttons" ]
-    [ button
-        [ onClick PreviousSlide,
-          disabled (model.index == 0) ]
-        [ text "<" ],
-      button
-        [ onClick NextSlide,
-          disabled (model.index == (Array.length model.slides - 1)) ]
-        [ text ">" ] ]
+
+renderControls : Model -> Html Msg
+renderControls model =
+    div [ class "row controls" ]
+        [ div
+            [ class "col-md-offset-10" ]
+            [ div [ class "pull-right" ]
+                [ button
+                    [ onClick PreviousSlide
+                    , class "btn btn-default glyphicon glyphicon-triangle-left"
+                    , disabled (model.index == 0)
+                    ]
+                    []
+                , span [] [ text " " ]
+                , button
+                    [ onClick NextSlide
+                    , class "btn btn-default glyphicon glyphicon-triangle-right"
+                    , disabled (model.index == (Array.length model.slides - 1))
+                    ]
+                    []
+                ]
+            ]
+        ]
+
 
 renderSlide : Model -> Html Msg
 renderSlide model =
-  div
-    [ class "slide" ]
-    [ Maybe.withDefault
-        (Markdown.toHtml [] "# Slide not found")
-        (Array.get
-          model.index
-          model.slides) ]
+    div [ class "row slide" ]
+        [ div [ class "row top" ] []
+        , div [ class "row" ]
+            [ div
+                [ class "col-md-offset-1 col-md-11" ]
+                [ Maybe.withDefault
+                    (Markdown.toHtml [] "# Slide not found")
+                    (Array.get
+                        model.index
+                        model.slides
+                    )
+                ]
+            ]
+        ]
+
 
 view : Model -> Html Msg
 view model =
-  let
-    imports = ["assets/main.css"]
-    stylesheet = Css.stylesheet imports []
-    width = (toString model.windowSize.width) ++ "px"
-    height = (toString model.windowSize.height) ++ "px"
-  in
+    let
+        imports =
+            [ "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+            , "assets/main.css"
+            ]
+
+        stylesheet =
+            Css.stylesheet imports []
+    in
     div
-      [ class "container",
-        style [ ("width", width), ("height", height) ] ]
-      [ Css.style [ Html.Attributes.scoped True ] stylesheet,
-        renderButtons model,
-        renderSlide model ]
+        [ class "container-fluid"
+        , style [ ( "height", "100%" ) ]
+
+        --, style [ ( "width", width ), ( "height", height ) ]
+        ]
+        [ Css.style [ Html.Attributes.scoped True ] stylesheet
+        , renderSlide model
+        , renderControls model
+        ]
--- src/Slides.elm
+++ src/Slides.elm
@@ -5,41 +5,56 @@ import Html exposing (Html)
 import Markdown exposing (defaultOptions, toHtmlWith)
 import String exposing (dropLeft, indexes, left)
 
+
 toHtml : String -> Html msg
 toHtml s =
-  toHtmlWith
-    { defaultOptions
-    | githubFlavored = Just { tables = True, breaks = False }
-    , sanitize = True }
-    []
-    s
+    toHtmlWith
+        { defaultOptions
+            | githubFlavored = Just { tables = True, breaks = False }
+            , sanitize = True
+        }
+        []
+        s
 
-rparse : String -> Array(Html msg) -> Array(Html msg)
+
+rparse : String -> Array (Html msg) -> Array (Html msg)
 rparse slides acc =
-  case indexes "# " slides of
-    [] -> push (toHtml slides) acc
-    _ :: [] -> push (toHtml slides) acc
-    _ :: next :: _ ->
-      let
-        slide = left next slides |> toHtml
-        remaining = dropLeft next slides
-      in
-        rparse
-          remaining
-          (push slide acc)
+    case indexes "# " slides of
+        [] ->
+            push (toHtml slides) acc
+
+        _ :: [] ->
+            push (toHtml slides) acc
+
+        _ :: next :: _ ->
+            let
+                slide =
+                    left next slides |> toHtml
+
+                remaining =
+                    dropLeft next slides
+            in
+            rparse
+                remaining
+                (push slide acc)
+
 
 dropLeadingHtmlTags : Int -> String -> String
 dropLeadingHtmlTags start slides =
-  dropLeft start slides
+    dropLeft start slides
 
-parse : String -> Array(Html msg)
+
+parse : String -> Array (Html msg)
 parse raw =
-  case indexes "# " raw of
-    [] -> empty
-    hd :: _ ->
-      let
-        slides = dropLeadingHtmlTags hd raw
-      in
-        rparse
-          slides
-          empty
+    case indexes "# " raw of
+        [] ->
+            empty
+
+        hd :: _ ->
+            let
+                slides =
+                    dropLeadingHtmlTags hd raw
+            in
+            rparse
+                slides
+                empty
